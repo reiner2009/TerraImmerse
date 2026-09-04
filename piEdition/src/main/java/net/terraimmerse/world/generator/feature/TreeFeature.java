ac5db6d4926e60129d92a2cf37c1f07bcb9857f9
reiner2009 <mathias009rhein@gmail.com>
2026-09-04 16:42:58 +0200

    .

--- piEdition/src/main/java/net/terraimmerse/world/generator/feature/TreeFeature.java
+++ piEdition/src/main/java/net/terraimmerse/world/generator/feature/TreeFeature.java
@@ -9,38 +9,51 @@ import java.util.Random;
 
 public class TreeFeature implements Feature{
     private Map<MaterialPos, String> tree;
+    private int treeH;
     public TreeFeature(){
         this.tree=new HashMap<>();
+        this.treeH=0;
     }
     @Override
     public void init(){
         this.tree.clear();
         Random random = new Random();
-        for(int i=0;i<5;i++){
-            this.tree.put(new MaterialPos(0,i,0), "wooden_log");
-        }
-        for(int x=-2;x<=2;x++){
-            for(int z=-2;z<=2;z++){
-                if(random.nextInt(10) < 8){
-                    this.tree.put(new MaterialPos(x,4,z), "leaves");
-                }
-            }
-        }
-        for(int x=-1;x<=1;x++){
-            for(int z=-1;z<=1;z++){
-                if(random.nextInt(10) < 8){
-                    this.tree.put(new MaterialPos(x,5,z), "leaves");
-                }
-            }
-        }
-        for(int x=-1;x<=1;x++){
-            for(int z=-1;z<=1;z++){
-                if(random.nextInt(10) < 8){
-                    this.tree.put(new MaterialPos(x,6,z), "leaves");
-                }
-            }
-        }
-        this.tree.put(new MaterialPos(0,7,0), "leaves");
+		this.treeH=random.nextInt(3)+4;
+		for(int x = -2; x < 3; x++){
+			for(int y = this.treeH-3; y < this.treeH-1; y++){
+				for(int z = -2; z < 3; z++){
+					this.tree.put(new MaterialPos(x,y,z), "leaves");
+				}
+			}
+		}
+		for(int y = 0; y < this.treeH; y++){
+			this.tree.put(new MaterialPos(0, y, 0), "wooden_log");
+		}
+		this.tree.put(new MaterialPos(0, this.treeH, 0), "leaves");
+		this.tree.put(new MaterialPos(1, this.treeH, 0), "leaves");
+		this.tree.put(new MaterialPos(-1, this.treeH, 0), "leaves");
+		this.tree.put(new MaterialPos(0, this.treeH, 1), "leaves");
+		this.tree.put(new MaterialPos(0, this.treeH, -1), "leaves");
+		this.tree.put(new MaterialPos(1, this.treeH-1, 0), "leaves");
+		this.tree.put(new MaterialPos(-1, this.treeH-1, 0), "leaves");
+		this.tree.put(new MaterialPos(0, this.treeH-1, 1), "leaves");
+		this.tree.put(new MaterialPos(0, this.treeH-1, -1), "leaves");
+		if(random.nextInt(10) > 1){
+			this.tree.put(new MaterialPos(-2, this.treeH-3, -2), "air");
+			this.tree.put(new MaterialPos(-2, this.treeH-2, -2), "air");
+		}
+		if(random.nextInt(10) > 1){
+			this.tree.put(new MaterialPos(-2, this.treeH-3, -2), "air");
+			this.tree.put(new MaterialPos(2, this.treeH-2, -2), "air");
+		}
+		if(random.nextInt(10) > 1){
+			this.tree.put(new MaterialPos(2, this.treeH-3, -2), "air");
+			this.tree.put(new MaterialPos(-2, this.treeH-2, -2), "air");
+		}
+		if(random.nextInt(10) > 1){
+			this.tree.put(new MaterialPos(2, this.treeH-3, -2), "air");
+			this.tree.put(new MaterialPos(2, this.treeH-2, -2), "air");
+		}
     }
     @Override
     public void place(MaterialPos treePos, Chunk chunk){
